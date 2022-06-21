4ac553d3aac87bd101880dd8c596031db3cb741d
Rubens Meirelles <rrubens.meirelles@gmail.com>
2022-06-21 18:42:06 -0300

    Adicionando novo index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,16 @@ function typeWriter(elemento){
 	const textoArray = elemento.innerHTML.split('')
 	elemento.innerHTML = ''
 	textoArray.forEach((letra, i) => {
-		setTimeout(() => elemento.innerHTML += letra, 100 * i)
+		setTimeout(() => elemento.innerHTML += letra, 150 * i)
 	})
 }
-const titulo = document.querySelector('h1')
+//const titulo = document.querySelector('h1 > span')
 typeWriter(titulo)
 
 function showZoom(){
 	document.querySelector('.zoomIn').style.opacity = '1';
 }
-setTimeout(showZoom, 1000)
+setTimeout(showZoom, 2000)
 
 /** MOSTRAR SETA */
 
